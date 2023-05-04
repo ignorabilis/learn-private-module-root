6a4501f47719e2f62cb068c3868fe55b7d86c212
ignorabilis <irina.yaroslavova@ignorabilis.com>
2023-05-04 07:15:43 +0300

    bump s3 webapp version

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 module "s3-webapp" {
   source  = "app.terraform.io/ignorabilis/s3-webapp/aws"
-  version = "1.0.0"
-  
+  version = "1.1.0"
+
   name    = var.name
   region  = var.region
   prefix  = var.prefix
